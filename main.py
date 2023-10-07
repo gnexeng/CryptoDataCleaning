a6958040ff2eacdbca56d5fbe8660ff796881b68
Guarionex salivia <gnexeng@gmail.com>
2023-10-07 12:02:22 -0500

    refactored all code and cleanup.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# To extract the following NIST tests from files in
-# C:\Users\gnexe\OneDrive\Documents\GitHub\coning-analysis\with-v2.0-fixes
-# ./coning-analysis/with-v2.0-fixes/
+# To extract the following NIST tests from files in https://github.com/gnexeng/CryptoDataCleaning.git
 
 # Tests: Frequency, BlockFrequency, CumulativeSums, Runs, LongestRun, Rank, FFT
 # OverlappingTemplate, Universal, ApproximateEntropy, Serial, LinearComplexity
@@ -13,8 +11,8 @@
 # file structure: 4,5-c3750-v2-0-0.txt  --->  103,203-c3750-v2-0-0.txt
 
 
-# path = f'C:\\Users\\gnexe\\OneDrive\\Documents\\GitHub\\coning-analysis\\with-v2.0-fixes\\'
-path = './coning-analysis/with-v2.0-fixes/'
+path = 'C:\\Users\\gnexe\\PycharmProjects\\CryptoDataCleaning\\coning-analysis\\with-v2.0-fixes\\'
+# path = './coning-analysis/with-v2.0-fixes/'
 tests = ['Frequency',
          'BlockFrequency',
          'CumulativeSums',
@@ -32,8 +30,8 @@ tests = ['Frequency',
 def create_list_f_vectors(x: int, y: int, total: int):
     """
     Creates a list of f-vectors starting such that (x,y)->(x+1,y+2)->...
-    :param x: starting value
-    :param y: starting value
+    :param x: starting value for f-vector dim 1
+    :param y: starting value for f-vector dim 2
     :param total: total number of f-vectors to generate
     :return: list of f-vectors as tuples
     """
@@ -44,41 +42,7 @@ def create_list_f_vectors(x: int, y: int, total: int):
     return f_vectors
 
 
-# ONLY RUN ONE TIME
-def create_files():
-    for val in tests:
-        extension = '.csv'
-        file = open(f'{val + extension}', 'w')
-        file.close()
-
-
-def testing_file(val):
-    in_file = open(path + f'3,3-c{val}-v2-0-0.txt', 'r')
-    for line in in_file:
-        try:
-            # if line.index(' Frequency'):
-            #     idx = line.find('0.')
-            #     print(line[idx: idx + 8])
-            if line.index(' 0.'):
-                idx = line.find(' 0.')
-                p_value = float(line[idx + 1:idx + 9])
-                # extract the name of test for this p_value
-                test_name = line.split(' ')[-1].strip()
-                print(p_value, test_name)
-                # open file test_name.csv
-                # append pair VAL, p-value to filename test_name.csv
-                # close file test_name.csv
-                # if test_name in tests:
-                #     print('true')
-                #     out_file = open(test_name + '.csv', 'a')
-                #     # append pair VAL, p-value to filename test_name.csv
-                #     out_file.write(f'{p_value},{val}')
-                #     # close file test_name.csv
-                #     out_file.close()
-        except ValueError:
-            continue
-
-def file_processing(file_name, val, vector):
+def file_processing(file_name, val, vector, file_name_modifier="", path_modifier=""):
     # For each line:
     for line in file_name:
         #    split line at spaces
@@ -89,7 +53,7 @@ def file_processing(file_name, val, vector):
                 test_name = line.split(' ')[-1].strip()  # extract the name of test for this p_value
                 # open file test_name.csv
                 if test_name in tests:
-                    out_file = open(test_name + '.csv', 'a')
+                    out_file = open(f'{test_name}-{file_name_modifier}.csv', 'a')
                     # append pair VAL, p-value to filename test_name.csv
                     out_file.write(f'{p_value},{val},{vector}\n')
                     # close file test_name.csv
@@ -97,32 +61,33 @@ def file_processing(file_name, val, vector):
         except ValueError:
             continue
 
-def extract_first_batch():
+
+def extract_coning_test():
+    global path
     # for each value VAL between 3750 - 3849
     for val in range(3750, 3850):
         # Open filename 3,3-cVAL-v2-0-0.txt at path
         in_file = open(path + f'3,3-c{val}-v2-0-0.txt', 'r')
+        file_processing(in_file, val=val, vector=(3, 3), file_name_modifier='coning')
+        in_file.close()
 
-        file_processing(in_file, val, (3, 3))
-    in_file.close()
 
-
-def extract_second_batch():
+def extract_vectors_test():
+    global path
     # for each f-vector between (4,5) ---> (103,203)
     f_vectors = create_list_f_vectors(4, 5, 100)
     for x, y in f_vectors:
         # Open filename x,y-c3750-v2-0-0.txt at path
         in_file = open(path + f'{x},{y}-c3750-v2-0-0.txt', 'r')
+        file_processing(in_file, val=3750, vector=(x, y), file_name_modifier='vectors')
+        in_file.close()
 
-        file_processing(in_file, 3750, (x, y))
-    in_file.close()
 
 def main():
-    # create_files()
-    # testing_file(3750)
-    extract_first_batch()
+    extract_coning_test()
+    extract_vectors_test()
     pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
